220236220a02cd884a96617af98d540e73a00bd6
Nihaal Sangha <nihaal.git@gmail.com>
2023-08-29 14:18:53 -0700

    Add proxyman-update

---
+++ Casks/proxyman-update.rb
@@ -0,0 +1,20 @@
+cask "proxyman-update" do
+  version "4.10.0,49100"
+  sha256 "f824c2ac1b4479fc9038f18824cdf7776e593c5f6b926ad0ff154314ad851e03"
+
+  url "https://download.proxyman.io/#{version.csv.second}/Proxyman_#{version.csv.first}.dmg"
+  name "Proxyman"
+  desc "HTTP debugging proxy"
+  homepage "https://proxyman.io/"
+
+  livecheck do
+    url "https://proxyman.io/osx/version.xml"
+    strategy :sparkle
+  end
+
+  auto_updates true
+
+  app "Proxyman.app"
+
+  uninstall quit: "com.proxyman.NSProxy"
+end
