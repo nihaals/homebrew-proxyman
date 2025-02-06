9a84ccbfcc3789a337b18ae42a009c0ecfbe8e64
Nihaal Sangha <nihaal.git@gmail.com>
2025-02-06 17:00:32 +0000

    Update Proxyman to v5.15.0

--- Casks/proxyman-update.rb
+++ Casks/proxyman-update.rb
@@ -1,6 +1,6 @@
 cask "proxyman-update" do
-  version "5.12.2,51202"
-  sha256 "d3308d0a3d41c76b6f998e34c2d8635b9e597fb8b85afdfc25e2ad5cca3dc27a"
+  version "5.15.0,51500"
+  sha256 "a6e50f5ddcae52d71c633b98264ae2eb5fe5adbf8575b779d33d889eb3fc1382"
 
   url "https://download.proxyman.io/#{version.csv.second}/Proxyman_#{version.csv.first}.dmg"
   name "Proxyman"
